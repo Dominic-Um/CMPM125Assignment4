b63a23fe1eede3cff3d8afabf441f8692d60b6e0
Dominic-Um <dumbrasa@ucsc.edu>
2026-05-25 22:40:29 -0700

    Revert "Added tree and boulder assets but did not place them in the scene this time."
    This reverts commit 19345beb17c7ffb33d4124a1133fa1b532924214.

--- Assets/Scripts/Systems/CallLoadScene.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CallLoadScene : MonoBehaviour
-{
-    public void LoadScene(string sceneName)
-    {
-        HandleSceneManager.instance.LoadScene(sceneName);
-    }
-}
